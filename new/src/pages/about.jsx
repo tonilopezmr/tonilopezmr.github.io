5964ae070a9c4320235055bf076472eb4db56fdf
tonilopezmr <toni@seaplane.io>
2023-08-07 01:46:56 +0200

    update about

--- new/src/pages/about.jsx
+++ new/src/pages/about.jsx
@@ -65,29 +65,20 @@ export default function About() {
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 12 years old, just two weeks
-                after arriving my first PC.
+                I’ve loved making things for as long as I can remember. 
+                I built many different projects since I was an 8-year-old kid, 
+                from selling flowers to making my own firecrackers. 
+                I've learned the ropes to make things happen. 
+                When I was 13 years old, my parents gifted me my first PC.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was Basketball.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                The only thing I loved more than computers as a kid was basketball. When I was 14, I began to play Counter-Strike Source and became a professional a few years later. One day, I discovered programming. I went to the DroidCon Android Community Meetup in Madrid, and I met interesting people and learned about many important development aspects.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                After being at DroidCon, I noticed two main things: everyone knew English and only MacBook. I sold everything and bought my first and last MacBook 15". No more Android Studio getting stuck with more than 6 projects open at the same time.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                After more than 10 years, I've created many personal projects and worked for many different companies. I've grown to a level I wouldn't have expected, being able to code in almost any language and use any platform from web to mobile, and any service.
               </p>
             </div>
           </div>
--- new/src/pages/articles/index.jsx
+++ new/src/pages/articles/index.jsx
@@ -45,7 +45,7 @@ export default function ArticlesIndex({ articles }) {
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
+        title="Writing on software design, product and company building."
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
