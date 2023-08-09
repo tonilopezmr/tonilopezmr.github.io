ca72ce4ab5a4794508edb6c6a7f2cf13a9aeaf1a
tonilopezmr <toni@seaplane.io>
2023-08-09 05:26:47 +0200

    Date down

--- new/src/components/ArticleLayout.jsx
+++ new/src/components/ArticleLayout.jsx
@@ -51,18 +51,18 @@ export function ArticleLayout({
               </button>
             )}
             <article>
-              <header className="flex flex-col">
+              <header className="flex flex-col">                
                 {meta.cover !== undefined ? <Image className="mt-10" src={meta.cover} /> : null}                
                 <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                  {meta.title}
-                </h1>
+                  {meta.title}                  
+                </h1>         
                 <time
                   dateTime={meta.date}
-                  className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+                  className="mt-3 flex items-center text-base text-zinc-400 dark:text-zinc-500"
                 >
                   <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
                   <span className="ml-3">{formatDate(meta.date)}</span>
-                </time>
+                </time>       
               </header>
               <Prose className="mt-8">{children}</Prose>
             </article>
